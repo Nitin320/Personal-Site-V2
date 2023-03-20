5bb98370c88d95f029f546589c1116152211427e
Nitin <nitinkm320@gmail.com>
2023-03-21 01:31:18 +0530

    basic navbar completed

--- app/cursor.ts
+++
@@ -1,6 +0,0 @@
-export{}
-
-    const cursor = document.querySelector('.cursor')
-      document.addEventListener('mousemove', c => {
-      cursor?.setAttribute("style", "top: "+(c.pageY - 10)+"px; left: "+(c.pageX - 10))
-      })
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,21 @@
 import { Inter } from 'next/font/google'
 import React, { useEffect, useState } from 'react'
 import './page.css'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
+      <Cursor/>
       <header>
-        <Cursor/>
+        <Navbar/>
       </header>
     </main>
   )
 }
+
 
 export function Cursor() {
 
@@ -22,12 +25,37 @@ export function Cursor() {
     document.addEventListener('mousemove', c => {
     cursor?.setAttribute("style", "top: "+(c.pageY - 10)+"px; left: "+(c.pageX - 10)+"px;")
     })
+    document.addEventListener('click', () => {
+      cursor?.classList.add('click-anim');
+      setTimeout(() => {
+      cursor?.classList.remove('click-anim');
+      },250)
+    })
   });
 
   return (
-    <div className="cursor-main">
-      <div id='cursor' className='cursor'></div>
+    <div>
+      <div id='cursor' className='cursor'>
+        <div className="pointer"></div>
+      </div>
     </div>
+  )
+}
+
+
+export function Navbar() {
+  return (
+
+    <div className="nav-main">
+      <img className='logo' src='/logo.png'/>
+      <div className="nav-items">
+        <a>About</a>
+        <a>My Expertise</a>
+        <a>Achievements</a>
+        <a>Contact</a>
+      </div>
+    </div>
+
   )
 }
 
